73a0c246a99c5d56b28947af6d923f57eb819f68
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-10-19 19:59:37 +0300

    added Kata-2, day-02.js

--- day-02.js
+++ day-02.js
@@ -40,6 +40,38 @@ function equal5() {
 
 //Basic data types--String
 //https://www.codewars.com/kata/571edea4b625edcb51000d8e
+var a1 = "A",
+  a2 = "a",
+  b1 = "B",
+  b2 = "b",
+  c1 = "C",
+  c2 = "c",
+  d1 = "D",
+  d2 = "d",
+  e1 = "E",
+  e2 = "e",
+  n1 = "N",
+  n2 = "n";
+function Dad() {
+  return d1 + a2 + d2;
+}
+function Bee() {
+  return b1 + e2 + e2;
+}
+function banana() {
+  return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+function answer1() {
+  return "no";
+}
+function answer2() {
+  return "no";
+}
+function answer3() {
+  return "yes";
+}
+
 
 //Basic data types--Array
 //https://www.codewars.com/kata/571effabb625ed9b0600107a
