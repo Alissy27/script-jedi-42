eb7f75fdeba9c28e178f5df02eef8f80468066cc
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-10-19 20:03:53 +0300

    update day-02.js, added "Basic data types--Object"

--- day-02.js
+++ day-02.js
@@ -97,3 +97,7 @@ function popElement(arr) {
 
 //Basic data types--Object
 //https://www.codewars.com/kata/571f1eb77e8954a812000837
+
+function animal(obj) {
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
