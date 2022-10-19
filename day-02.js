ab43c38830a763fcecf61e0f74a5cc0708dbc875
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-10-19 18:14:58 +0300

    created day-02.js, added Kata number

---
+++ day-02.js
@@ -0,0 +1,48 @@
+//Basic data types--Number
+//https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
+
+let v1 = 50,
+  v2 = 100,
+  v3 = 150,
+  v4 = 200,
+  v5 = 2,
+  v6 = 250;
+
+function equal1() {
+  let a = v1,
+    b = v1;
+  return a + b;
+}
+
+function equal2() {
+  let a = v3,
+    b = v1;
+  return a - b;
+}
+
+function equal3() {
+  let a = v1,
+    b = v5;
+  return a * b;
+}
+
+function equal4() {
+  let a = v4,
+    b = v5;
+  return a / b;
+}
+
+function equal5() {
+  let a = v6,
+    b = v3;
+  return a % b;
+}
+
+//Basic data types--String
+//https://www.codewars.com/kata/571edea4b625edcb51000d8e
+
+//Basic data types--Array
+//https://www.codewars.com/kata/571effabb625ed9b0600107a
+
+//Basic data types--Object
+//https://www.codewars.com/kata/571f1eb77e8954a812000837
