08239b1c608088f03bbb95d843b475fdc07aa1d9
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-10-21 12:27:59 +0300

    update day-06.js, added 2 solution of 3

--- day-06.js
+++ day-06.js
@@ -20,3 +20,13 @@ function maxMultiple(divisor, bound) {
   return d;
 }
 
+// https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+
+// no solution yet
+
+//https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+function unusualFive() {
+  let str = "fives";
+  return str.length;
+}
