57f8c737e249d34f2bb10790b01f3679f138669e
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-10-26 17:12:56 +0300

    update day-09.js. added 2 solution of 3

--- day-09.js
+++ day-09.js
@@ -21,3 +21,11 @@ function cutIt(arr) {
 }
 
 // https://www.codewars.com/kata/57277a31e5e51450a4000010
+
+function firstToLast(str, c) {
+  if (str.search(c) !== -1) {
+    return str.lastIndexOf(c) - str.indexOf(c);
+  } else {
+    return -1;
+  }
+}
