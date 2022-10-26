0fcbcbbf8193cc6f3afe030e40889e8e495112c7
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-10-26 16:50:28 +0300

    create day-09.js, added 1 solution of 3

---
+++ day-09.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
+
+function cutIt(arr) {
+  const shortestString = arr.reduce(function (shortest, currentWord) {
+    let shortestWord;
+    if (shortest.length < currentWord.length) {
+      const shortestWord = shortest;
+      return shortestWord;
+    } else {
+      shortestWord = currentWord;
+      return shortestWord;
+    }
+    return shortestWord.length;
+  });
+
+  const cutedArray = arr.map(function (elem) {
+    return elem.substring(0, shortestString.length);
+  });
+
+  return cutedArray;
+}
+
+// https://www.codewars.com/kata/57277a31e5e51450a4000010
