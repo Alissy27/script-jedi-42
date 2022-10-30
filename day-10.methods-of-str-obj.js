3961b14f59a0c6a62ee9216f49a53930ebfaf522
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-10-30 22:06:38 +0300

    update day-10.js, added solution 3 of 3

--- day-10.methods-of-str-obj.js
+++ day-10.methods-of-str-obj.js
@@ -14,8 +14,39 @@ function alienLanguage(str) {
 }
 
 // https://www.codewars.com/kata/57284d23e81185ae6200162a
+//tests passed, answers no
 
-// NO SOLUTION YET
+function topSecret(str) {
+  const arrFromString = str.split("");
+
+  const res = [];
+  for (let i = 0; i < arrFromString.length; i++) {
+    if (
+      arrFromString[i].charCodeAt() > 68 &&
+      arrFromString[i].charCodeAt() < 122 &&
+      arrFromString[i].charCodeAt() !== 97 &&
+      arrFromString[i].charCodeAt() !== 98 &&
+      arrFromString[i].charCodeAt() !== 99
+    ) {
+      res.push(String.fromCharCode(arrFromString[i].charCodeAt() - 3));
+    } else if (arrFromString[i].charCodeAt() === 65) {
+      res.push(String.fromCharCode(88));
+    } else if (arrFromString[i].charCodeAt() === 66) {
+      res.push(String.fromCharCode(89));
+    } else if (arrFromString[i].charCodeAt() === 67) {
+      res.push(String.fromCharCode(90));
+    } else if (arrFromString[i].charCodeAt() === 97) {
+      res.push(String.fromCharCode(120));
+    } else if (arrFromString[i].charCodeAt() === 98) {
+      res.push(String.fromCharCode(121));
+    } else if (arrFromString[i].charCodeAt() === 99) {
+      res.push(String.fromCharCode(122));
+    } else {
+      res.push(String.fromCharCode(arrFromString[i].charCodeAt()));
+    }
+  }
+  return res.join("");
+}
 
 //  https://www.codewars.com/kata/5729b103dd8bac11a900119e/train/javascript
 
