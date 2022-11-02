b46f2865af8b2d4a864f6ef3767df2d876843a0a
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-11-02 23:10:13 +0200

    create TDD Bins.entries().js (without 1 solution)

---
+++ TDD Bins/TDD Bins.entries().js
@@ -0,0 +1,28 @@
+// http://tddbin.com/#?kata=es6/language/array-api/entries
+
+// 41: array - entries
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`[].entries()` returns an iterator object with all entries', function() {
+  it('returns key+value for each element', function() {
+    const arr = ['a', 'b', 'c'];
+    const entriesAsArray = Array.from(arr.entries());
+    assert.deepEqual(entriesAsArray, [[0,"a"], [1,"b"], [2,"c"]]);
+  });
+  it('empty elements contain the value `undefined`', function() {
+    const arr = ['one'];
+    arr[2] = 'three';
+    const secondValue = [...arr.entries()][1];
+    assert.deepEqual(secondValue, [1, void 0]);
+  });
+  
+  // without solution
+  describe('returns an iterable', function() {
+    it('has `next()` to iterate', function() {
+      const arr = ['one'];
+      const value = Array.from(arr).next();
+      assert.deepEqual(value, [0, 'one']);
+    });
+  });
+});
