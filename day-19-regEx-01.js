30784fbf290265b033f50efc5296f8c000155cc0
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-11-07 14:23:40 +0200

    update day-19.js, added solution 2 of 2

--- day-19-regEx-01.js
+++ day-19-regEx-01.js
@@ -13,4 +13,22 @@ function countAnimals(animals, count) {
   return res;
 }
 
-// 
+// https://www.codewars.com/kata/573975d3ac3eec695b0013e0/train/javascript
+
+function findSimilarity(str, word) {
+  const arrOfStr = str.split(" ");
+  let newStr = "";
+  const firstLetter = word.slice(0, 1);
+  const lastLetter = word.slice(-1);
+  for (let findWord of arrOfStr) {
+    if (findWord.length === word.length) {
+      if (
+        firstLetter === findWord.slice(0, 1) &&
+        lastLetter === findWord.slice(-1)
+      ) {
+        newStr += findWord + " ";
+      }
+    }
+  }
+  return newStr.trim();
+}
