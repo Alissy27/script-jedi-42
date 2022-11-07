6e72b01636812653a3e4baf7047605a0cd7e5d39
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-11-07 17:02:56 +0200

    create day-20.js. added solution 1 of 2

---
+++ day-20-regEx-02.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/573bca07dffc1aa693000139/train/javascript
+
+var regex=/^-?[9][0-9?]{0,5}[0]{4,}$/
+
+// https://www.codewars.com/kata/573bca07dffc1aa693000139/train/javascript
