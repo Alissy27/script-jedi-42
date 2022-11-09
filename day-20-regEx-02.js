f47474ec606b9c1d5bbebcffabece187c3dd8d61
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-11-10 01:09:04 +0300

    update day-20.js. added solution 2 of 2

--- day-20-regEx-02.js
+++ day-20-regEx-02.js
@@ -1,6 +1,7 @@
 // https://www.codewars.com/kata/573bca07dffc1aa693000139/train/javascript
 
-var regex=/^-?[9][0-9?]{0,5}[0]{4,}$/
+const regex=/^-?[9][0-9?]{0,5}[0]{4,}$/
 
 // https://www.codewars.com/kata/573d11c48b97c0ad970002d4
 
+const regex = /[h][t][t][p][s]?[:][\/]{2}((?:[\w-]+)(?:\.[\w-]+)+)?[a-zA-Z0-9]+[.][c|n][o|e][m|t]/ig
