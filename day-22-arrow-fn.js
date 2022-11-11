8f8867e74f9e72a549d0938c7c387a8b2df55878
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-11-11 12:25:10 +0300

    create day-22.js, added solutions 2 of 2

---
+++ day-22-arrow-fn.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
+
+function anyArrows(arrows) {
+  return arrows.some((arrows) => arrows.damaged !== true);
+}
+
+// https://www.codewars.com/kata/586909e4c66d18dd1800009b/train/javascript
+
+const multiplyAll = (arr) => (num) => {
+  return arr.map((elem) => elem * num);
+};
