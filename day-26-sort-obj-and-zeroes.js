00bee30412c4e46e9442e31f3716b6da6b42e981
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-11-16 14:46:54 +0200

    update day-26.js. added solution 2 of 2

--- day-26-sort-obj-and-zeroes.js
+++ day-26-sort-obj-and-zeroes.js
@@ -3,3 +3,16 @@
 const OrderPeople = function(people){
   return people.sort( (b, a) => (b.age - a.age)); 
 }
+
+
+// https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
+
+function moveZeros(arr) {
+  const arrWithoutZeroes = [];
+  const arrZeroes = [];
+  arr.forEach((elem) => {
+    elem !== 0 ? arrWithoutZeroes.push(elem) : arrZeroes.push(elem);
+  });
+
+  return [...arrWithoutZeroes, ...arrZeroes];
+}
