0bea3338a003c02ca9cd08aa2906f76b656c3fd2
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-11-16 15:04:36 +0200

    create day-27.js, added solution 1 of 2

---
+++ day-27-closures.js
@@ -0,0 +1,16 @@
+// https://classroom.google.com/w/NTM3MzM5MjEwNzk3/tc/NTM3MzM5MjEwODA5
+
+
+
+
+// https://www.codewars.com/kata/514aa0dc21607ae236000017/train/javascript
+
+const greet_abe = function () {
+  const name = "Abe";
+  return "Hello, " + name + "!";
+};
+
+const greet_ben = function () {
+  const name = "Ben";
+  return "Hello, " + name + "!";
+};
