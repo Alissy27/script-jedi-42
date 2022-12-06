2f2f677cf69972c9d9ed622b4f1bda9b3001095e
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-12-06 19:08:10 +0200

    create day-35.js, added solutions

---
+++ day-35-oop-prototypes-class-bind.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
+
+Object.prototype.description = "TODO: GIVE ME A DESCRIPTION" 
+
+
+// https://www.codewars.com/kata/javascript-class-like-objects
+
+class Animal {
+  constructor(name, type) {
+    (this.name = name), (this.type = type);
+  }
+  toString() {
+    return this.name + " is a " + this.type;
+  }
+}
+
+
+// https://www.codewars.com/kata/power-bind
+
+Function.prototype.bind = function (ctx) {
+  const func = this;
+  return function (...args) {
+    const rightCtx = this === global ? ctx : this;
+    return func.apply(rightCtx, args);
+  };
+};
