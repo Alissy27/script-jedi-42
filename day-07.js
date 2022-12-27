bffb82f226afd75c489ef87c8dc8cb8d69ed6880
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-12-27 20:20:10 +0200

    Update day-07.js

--- day-07.js
+++ day-07.js
@@ -1,6 +1,22 @@
 // https://www.codewars.com/kata/55e7650c8d894146be000095/train/javascript
 
 function validateMessage(msg) {
+  if (msg == null) {
+    throw new ReferenceError("Message is null!");
+  }
+  if (typeof msg !== "string") {
+    throw new TypeError(
+      `Message should be of type string but was of type ${typeof msg}!`
+    );
+  }
+  if (msg.length > 255 || msg.length === 0) {
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+  }
+
+  return !(msg.includes("<") && msg.includes(">"));
+}
+
+function validateMessage(msg) {
   if (/<.*>/.test(msg)) {
     return false;
   } else if (msg === null) {
