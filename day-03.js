51e1e834fd3136ffd204e5725284a3e0dc584199
Elena Tarasova <107623856+Alissy27@users.noreply.github.com>
2022-12-27 15:19:40 +0200

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -1,6 +1,10 @@
 // https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
 
 function trueOrFalse(val) {
+  return Boolean(val).toString();
+}
+
+function trueOrFalse(val) {
   if (val == false) {
     return "false";
   } else if (val == null && val == undefined) {
@@ -11,6 +15,10 @@ function trueOrFalse(val) {
 }
 
 // https://www.codewars.com/kata/57202aefe8d6c514300001fd
+
+function saleHotdogs(n) {
+  return n < 5 ? n * 100 : n < 10 ? n * 95 : n * 90;
+}
 
 function saleHotdogs(n) {
   if (n < 5) {
